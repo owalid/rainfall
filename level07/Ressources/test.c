fcbe213f27f52b157dc008f4d4d2003f15194da8
owalid <othmane.el.ayadi@gmail.com>
2021-07-09 12:55:44 +0200

    add liveoverflow program to test

---
+++ level07/Ressources/test.c
@@ -0,0 +1,27 @@
+struct internet {
+    int priority;
+    char *name;
+};
+
+void winner()
+{
+    printf("hello %d\n", time(NULL));
+}
+
+int main(int argc, char **argv)
+{
+    struct internet *i1, *i2, *i3;
+
+    i1 = malloc(sizeof(struct internet));
+    i1->priority = 1;
+    i1->name = malloc(8);
+
+    i2 = malloc(sizeof(struct internet));
+    i2->priority = 1;
+    i2->name = malloc(8);
+
+    strcpy(i1->name, argv[1]);
+    strcpy(i2->name, argv[2]);
+
+    printf("and that's a wrap folks ! \n");
+}
