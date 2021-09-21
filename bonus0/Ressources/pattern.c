537f345b9984161e8b7531893da3e171e99a1348
Sawyerf <sawyer.flink@protonmail.ch>
2021-09-21 11:48:51 +0200

    Add: pattern bonus0

--- bonus0/Ressources/pattern.c
+++ bonus0/Ressources/pattern.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
+# include <stdio.h>
 #include <unistd.h>
 
 int main()
 {
-		printf("\x80");
-		printf("BBBBBBBBBBBB");
-		printf("\x76\xd5\xff\xff");
-		printf("BBBBBBBBBBBBBBBBBBBBBB\n");
+		printf("\x80" \
+			"BBBBBBBBBBBB" \
+			"\x76\xd5\xff\xff" \
+			"BBBB" \
+			"\n");
 }
